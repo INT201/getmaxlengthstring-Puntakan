0381187285145caee444276764597a4194b5a991
Puntakan <puntakan.c@mail.kmutt.ac.th>
2022-09-14 22:08:05 +0700

    Done

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,24 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
-}
+  let arr = new Array()
+  arr = arrayOfString
+  let max = new Array()
+  if (arrayOfString === null || arrayOfString === undefined) {
+    return undefined
+  }
+    for (let j = 0; j < arr.length; j++) {
+      let word = [...arr[j]]
+      if (word.length > max.length) {
+        max = arr[j]
+      }
+      else if (word.length < max.length) {
+        max = max
+      }
+      else if (word.length === max.length){
+        return [max, arr[j]]
+        }
+      }
+    return [max]
+  }
 module.exports = getMaxLengthString
